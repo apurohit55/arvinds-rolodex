0b677d1090fb7c9cd80ab8cfbeb889cf15004827
Arvind Purohit <arvindgpurohit@gmail.com>
2020-03-14 03:52:25 -0400

    Updates

---
+++ precache-manifest.647c058ea4c41458b463c1eea3275e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5320aa39434874d71522351cdfa2078",
+    "url": "/https/apurohit55.github.io/arvinds-rolodex/index.html"
+  },
+  {
+    "revision": "daac4c866eb50f377fec",
+    "url": "/https/apurohit55.github.io/arvinds-rolodex/static/css/main.66c3ccac.chunk.css"
+  },
+  {
+    "revision": "674ca8ce9b5291ffa46a",
+    "url": "/https/apurohit55.github.io/arvinds-rolodex/static/js/2.6130c845.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/https/apurohit55.github.io/arvinds-rolodex/static/js/2.6130c845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daac4c866eb50f377fec",
+    "url": "/https/apurohit55.github.io/arvinds-rolodex/static/js/main.64a23b9e.chunk.js"
+  },
+  {
+    "revision": "f0a5ef87877b59c16e09",
+    "url": "/https/apurohit55.github.io/arvinds-rolodex/static/js/runtime-main.0c6916d3.js"
+  }
+]);
